15b1284d936c288964259fd2d93253da5a40f9e9
Chris Tomsett <90329606+christomsett@users.noreply.github.com>
2025-06-25 13:11:30 +0100

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,90 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-pano-platformtelescopes-zodiacallight-cc",
+      "name": "360-pano-platformtelescopes-zodiacallight-CC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6726316445469545,
+          "pitch": -0.19359834186774982,
+          "rotation": 0,
+          "target": "1-360-pano-uarizona-bok-2-3m-telescope-007-cc"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.721805302426974,
+          "pitch": -0.12158524682587135,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-360-pano-uarizona-bok-2-3m-telescope-007-cc",
+      "name": "360-pano-UArizona-Bok-2-3m-Telescope-007-CC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.8240189923712542,
+          "pitch": -0.20962529959581921,
+          "title": "Hi",
+          "text": "ssss&nbsp;https://esas.soton.ac.uk/"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
